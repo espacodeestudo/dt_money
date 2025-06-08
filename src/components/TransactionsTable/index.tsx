264bb1d98dc7c0cd5adacf786865df69d6ee24eb
yuransimao <yuransimao7@gmail.com>
2025-06-08 14:04:41 +0100

    update

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -6,7 +6,7 @@ import { Container } from './style'
 
 function TransactionsTable() {
    const {transactions}= useContext(TransactionContext)
-
+    
    
    
   return (
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5173/api'
+    : 'https://dtmoneys.netlify.app/api'
+
+
 export const api = axios.create({
-    baseURL: 'http://localhost:5173/api'
+    baseURL
 })
